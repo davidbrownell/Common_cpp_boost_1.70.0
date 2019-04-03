044a1d546c1170d24e5053e3b8ddc6d5cf802699
David Brownell <db@DavidBrownell.com>
2019-04-02 21:18:24 -0700

    Added unit tests

---
+++ src/UnitTests/Serialization_UnitTest.cpp
@@ -0,0 +1,22 @@
+/////////////////////////////////////////////////////////////////////////
+///
+///  \file          Serialization_UnitTest.cpp
+///  \brief         Unit test for Serializaiton.h using this version of boost
+///
+///  \author        David Brownell <db@DavidBrownell.com>
+///  \date          2019-03-29 15:07:11
+///
+///  \note
+///
+///  \bug
+///
+/////////////////////////////////////////////////////////////////////////
+///
+///  \attention
+///  Copyright David Brownell 2019
+///  Distributed under the Boost Software License, Version 1.0. See
+///  accompanying file LICENSE_1_0.txt or copy at
+///  http://www.boost.org/LICENSE_1_0.txt.
+///
+/////////////////////////////////////////////////////////////////////////
+#include <BoostCommon/UnitTestsImpl/Serialization_UnitTestImpl.hpp>
